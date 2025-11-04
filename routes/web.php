fb6c9265fa674c25d64fc3a7c39fc09a4ba8c2fd
ayelen <sosaayelcamila@gmail.com>
2025-11-04 23:28:10 +0000

    feat: Agregar rutas del módulo Productos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas de Productos
+Route::resource('productos', ProductoController::class);
+Route::get('productos/{producto}/stock', [ProductoController::class, 'stock'])->name('productos.stock');
