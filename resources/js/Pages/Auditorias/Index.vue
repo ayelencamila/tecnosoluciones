9775ad699f8bb37ebd3dc1372cff9bc1db397e35
ayelen <sosaayelcamila@gmail.com>
2025-11-20 20:30:09 +0000

    feat: se implemento vista inicial de Auditorias

---
+++ resources/js/Pages/Auditorias/Index.vue
@@ -0,0 +1,7 @@
+<template>
+    <AppLayout>
+        <template #header>Auditoría</template>
+        <div class="py-12"><div class="max-w-7xl mx-auto sm:px-6 lg:px-8">Próximamente: Historial de Cambios</div></div>
+    </AppLayout>
+</template>
+<script setup> import AppLayout from '@/Layouts/AppLayout.vue'; </script>
